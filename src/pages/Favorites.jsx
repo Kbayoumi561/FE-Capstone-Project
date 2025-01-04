abfe1e1dbb48c4493695943483d042723a96c0cb
Kbayoumi561@gmail.com <kbayoumi56@gmail.com>
2025-01-04 19:05:57 +0200

    fix layout and apply updates

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,7 +2,7 @@ const Favorites = () => {
   return (
     <div className="container mx-auto p-4 text-center">
       <h1 className="text-2xl font-bold mb-4">Your Favorite Recipes</h1>
-      <p className="text-gray-600">You haven’t added any favorite recipes yet.</p>
+      <p className="text-gray-600">You havent added any favorite recipes yet.</p>
     </div>
   );
 };
