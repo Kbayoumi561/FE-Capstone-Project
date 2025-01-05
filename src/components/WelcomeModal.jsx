781e6f107fe67b1200b8b33da1d4a4703d4f5b81
Kbayoumi561@gmail.com <kbayoumi56@gmail.com>
2025-01-06 01:08:31 +0200

    Resolving the building issue related to image path

--- src/components/WelcomeModal.jsx
+++ src/components/WelcomeModal.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
-import foodImage from 'src/assets/food.jpg';
+import foodImage from 'src/assets/foodImage.jpg';
 
 const WelcomeModal = ({ onClose }) => {
   useEffect(() => {
